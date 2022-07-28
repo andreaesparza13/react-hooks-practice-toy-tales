009091de3c935cdce81fac38db69b3112b1653ce
Andrea <acesparza05@gmail.com>
2022-07-28 12:56:30 -0500

    commit tree a79a7f00572d31e9125d9a8193e855a86162e26d

--- src/components/ToyForm.js
+++ src/components/ToyForm.js
@@ -1,11 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 
-function ToyForm() {
+function ToyForm({ toys, setToys }) {
+
+  const [name, setName] = useState("")
+  const [image, setImage] = useState("")
+  // const [likes, setLikes] = useState(0)
+
+  function addToy() {
+    const postReqObj = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accepts: "application/json"
+      },
+      body: JSON.stringify({
+        name,
+        image,
+        // likes
+      })
+    }
+    fetch("http://localhost:3001/toys", postReqObj)
+      .then(res => res.json())
+      .then(newToy => {
+        setToys([...toys, newToy]);
+      })
+  }
+
   return (
     <div className="container">
-      <form className="add-toy-form">
+      <form className="add-toy-form" onSubmit={addToy}>
         <h3>Create a toy!</h3>
         <input
+          onChange={(e) => setName(e.target.value)}
           type="text"
           name="name"
           placeholder="Enter a toy's name..."
@@ -13,6 +39,7 @@ function ToyForm() {
         />
         <br />
         <input
+          onChange={(e) => setImage(e.target.value)}
           type="text"
           name="image"
           placeholder="Enter a toy's image URL..."
